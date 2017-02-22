2600408de4b470843594db17ec549f12874ce090
ShaneDiNozzo <shanedinozzo@gmail.com>
2017-02-22 22:16:08 +0100

    Update main code

--- FxDrctRPG.js
+++ FxDrctRPG.js
@@ -1,3 +1,6 @@
+app.LoadScript("progressbar.js");
+app.LoadScript("character.js");
+app.LoadScript("monsters.js");
 
 var nl = "\n";
 
@@ -7,99 +10,90 @@ function OnStart() {
   chartr = new Character("Shane");
   mnr = new Encounter();
   
+  txtMobName = app.CreateText("");
+  layMain.AddChild(txtMobName);
+  
+  txtMobHP = app.CreateText("");
+  layMain.AddChild(txtMobHP);
+  
+  txtMobSTR = app.CreateText("");
+  layMain.AddChild(txtMobSTR);
+  
+  btnEncounter = app.CreateButton("encounter");
+  btnEncounter.SetOnTouch(function() {
+    mnr.DoEncounter();
+  });
+  layMain.AddChild(btnEncounter);
   
   btn = app.CreateButton("hit");
   btn.SetOnTouch(function() {
-    //
+    chartr.HP = chartr.HP - mnr.mob.Hit();
+    if(chartr.HP <= 0) {
+      txtCharacterHP.SetText("DIED!");
+      txtCharacterHP.SetTextColor("red");
+    } else {
+      txtCharacterHP.SetText("HP: " + chartr.HP);
+    }
+    prgCharacterHP.Update(chartr.HP);
   });
   layMain.AddChild(btn);
   
-  txtCharacterName = app.CreateText(chartr.Name);
+  txtCharacterHP = app.CreateText("HP: " + chartr.HP);
+  layMain.AddChild(txtCharacterHP);
+  
+  prgCharacterHP = new Progressbar(chartr.HP, 0.8);
+  layMain.AddChild(prgCharacterHP.Create());
+  prgCharacterHP.ForegroundColor = "red";
+  prgCharacterHP.Update(chartr.HP);
+  
+  txtCharacterXP = app.CreateText("XP: " + chartr.XP);
+  layMain.AddChild(txtCharacterXP);
+  
+  prgCharacterXP = new Progressbar(chartr.XP, 0.8);
+  layMain.AddChild(prgCharacterXP.Create());
+  prgCharacterXP.Update(chartr.XP);
+  
+  txtCharacterName = app.CreateText(chartr.Name, 0.8, -1, "Right");
   layMain.AddChild(txtCharacterName);
   
-  txtCharacterHP = app.CreateText(chartr.HP);
-  layMain.AddChild(txtCharacterHP);
+  txtCharacterLevel = app.CreateText("LVL: ", 0.8, -1, "Left");
+  layMain.AddChild(txtCharacterLevel);
   
   app.AddLayout(layMain);
 }
 
-class Encounter {
+
+
+function showData() {
+  txtMobName.SetText("Name: " + mnr.mob.Name);
+  txtMobHP.SetText("HP: " + mnr.mob.HP);
+  txtMobSTR.SetText("STR: " + mnr.mob.STR);
+}
+
+class Randoms {
+  constructor(minimum, maximum) {
+    this.minimum = minimum;
+    this.maximum = maximum;
+  }
+  
+  static Half(value) {
+    var minimum = 50 / 100 * value;
+    return Math.floor(Math.random() * (value - minimum + 1)) + minimum;
+  }
+  
+  static MaxMin(maximum, minimum) {
+    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  }
+}
+
+class MobSpecies {
   constructor() {
-    this.mobNames = ["Retooth", "Werewolf", "Mergareph"];
-    this.mob = "";
+    this.diffculty = "";
   }
   
-  get monster() { return this.mob; }
-  
-  set monster(mob) { mob = this.mob }
-  
-  GetRandomMobName() {
-    var minimum = 1;
-    return Math.floor(Math.random() * (mobNames.length() - minimum + 1)) + minimum;
-  }
-  
-  DoEncounter() {
-    this.mob = new Mob(mobNames[GetRandomMobName], 10, 2);
-  }
-  
-  MonsterName() {
-    return this.mob.Name;
+  Lynx() {
+    var name = "Lynx";
+    var hp = Randoms.MaxMin(10, 7);
   }
 }
 
-class Mob {
-  constructor(name, hp, str) {
-    this.name = name;
-    this.hp = hp;
-    this.str = str;
-  }
-  
-  get Name() { return this.name; }
-  get HP()   { return this.hp;   }
-  get STR()  { return this.str;  }
-  
-  set Name(name) { this.name = name; }
-  set HP(hp)     { this.hp   = hp;   }
-  set STR(str)   { this.str  = str;  }
-  
-  Hit() {
-    var minimum = this.str - 5;
-    return Math.floor(Math.random() * (this.str - minimum + 1)) + minimum;
-  }
-  
-  GetHit(characterHit) {
-    this.hp = this.hp - characterHit;
-  }
-}
-
-class Character {
-  constructor(name) {
-    this.name = name;
-    this.hp = 100;
-    this.str = 10;
-    this.xp = 0;
-  }
-  
-  get Name() { return this.name; }
-  get HP()   { return this.hp;   }
-  get STR()  { return this.str;  }
-  get XP()   { return this.xp;   }
-  
-  set Name(name) { this.name = name; }
-  set HP(hp)     { this.hp   = hp;   }
-  set STR(str)   { this.str  = str;  }
-  set XP(xp)     { this.xp   = xp;   }
-  
-  Hit(foeHP) {
-    var minimum = this.str - 5;
-    return Math.floor(Math.random() * (this.str - minimum + 1)) + minimum;
-  }
-  
-  GetHit(foeHit) {
-    this.hp = this.hp - foeHit;
-  }
-  
-  AddXP(xp) {
-    this.xp = this.xp + xp;
-  }
-}
